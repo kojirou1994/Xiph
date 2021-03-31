960478d119c3904fbd733d8413eab10dc4a82f6f
kojirou <kojirouhtc@gmail.com>
2021-03-31 16:01:16 +0800

    indent

--- Sources/Flac/FlacMetadataLevel0.swift
+++ Sources/Flac/FlacMetadataLevel0.swift
@@ -1,24 +1,24 @@
 public enum FlacMetadataLevel0 {
 
-    public static func getStreamInfo(filename: String) throws -> FlacStreamMetadata {
+  public static func getStreamInfo(filename: String) throws -> FlacStreamMetadata {
     let ptr = UnsafeMutablePointer<FLAC__StreamMetadata>.allocate(capacity: 1)
     try preconditionOrThrow(FLAC__metadata_get_streaminfo(filename, ptr).cBool)
     return .init(ptr)
   }
 
-    public static func getTags(filename: String) throws -> FlacStreamMetadata {
+  public static func getTags(filename: String) throws -> FlacStreamMetadata {
     var ptr: UnsafeMutablePointer<FLAC__StreamMetadata>?
     try preconditionOrThrow(FLAC__metadata_get_tags(filename, &ptr).cBool)
     return .init(ptr.unsafelyUnwrapped)
   }
 
-    public static func getCueSheet(filename: String) throws -> FlacStreamMetadata {
+  public static func getCueSheet(filename: String) throws -> FlacStreamMetadata {
     var ptr: UnsafeMutablePointer<FLAC__StreamMetadata>?
     try preconditionOrThrow(FLAC__metadata_get_cuesheet(filename, &ptr).cBool)
     return .init(ptr.unsafelyUnwrapped)
   }
 
-    public static func getPicture(
+  public static func getPicture(
     filename: String,
     ptype: FLAC__StreamMetadata_Picture_Type?,
     mimeType: UnsafePointer<Int8>!,
